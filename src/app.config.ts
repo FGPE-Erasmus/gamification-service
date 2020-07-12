b9784d31b25d0f4864897ad4dfcb725ca7ce9899
José C. Paiva <josepaiva94@gmail.com>
2020-07-12 18:23:08 +0100

    fix mongodb windows

--- src/app.config.ts
+++ src/app.config.ts
@@ -36,6 +36,7 @@ export const appConfig: IAppConfig = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    authSource: process.env.DB_AUTH,
 
     synchronize: true,
     useUnifiedTopology: true,
