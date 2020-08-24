e448790ca3a3fa39e29890d83bf659e91db67125
hvsio <h4sio@o2.pl>
2020-08-24 14:36:47 +0200

    initial rewards

--- src/virtual-item/virtual-item.resolver.ts
+++ src/virtual-item/virtual-item.resolver.ts
@@ -2,10 +2,16 @@ import { VirtualItemService } from './virtual-item.service';
 import { VirtualItemEntity as VirtualItem } from './entities/virtual-item.entity';
 import { Args, Resolver, Query } from '@nestjs/graphql';
 import { GqlUser } from 'src/common/decorators/gql-user.decorator';
+import { PlayerEntity as Player } from 'src/player/entities/player.entity';
 
 @Resolver()
 export class VirtualItemResolver {
   constructor(readonly virtualItemService: VirtualItemService) {}
+
+  @Query(() => [Player])
+  async playersWithVirtualItem(@Args('id') id: string): Promise<any[]> {
+    return await this.virtualItemService.getPlayersWithVirtualItem(id);
+  }
 
   @Query(() => [VirtualItem])
   async virtualItem(@Args('rewardId') id: string, @GqlUser('id') playerId: string): Promise<VirtualItem[]> {
@@ -16,9 +22,4 @@ export class VirtualItemResolver {
   async virtualItems(@GqlUser('id') playerId: string): Promise<VirtualItem[]> {
     return await this.virtualItemService.getVirtualItems(playerId);
   }
-
-  // @Query(() => [Player])
-  // async playersWithVirtualItem(@Args('id') id: string): Promise<Player[]> {
-  //     return await this.virtualItemService.getPlayersWithVirtualItem(id);
-  // }
 }
