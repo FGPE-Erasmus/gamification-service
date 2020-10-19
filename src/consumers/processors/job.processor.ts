ca58e9cf03e23349b4c635ed28a1b64aff3b0b8d
hvsio <h4sio@o2.pl>
2020-10-19 21:43:50 +0200

    Actions vol2

--- src/consumers/processors/job.processor.ts
+++ src/consumers/processors/job.processor.ts
@@ -12,14 +12,18 @@ import { PlayerRewardService } from 'src/player-reward/player-reward.service';
 import { PlayerReward } from 'src/player-reward/models/player-reward.model';
 import { ChallengeStatusService } from 'src/challenge-status/challenge-status.service';
 import { State } from 'src/challenge-status/models/state.enum';
+import { RewardType } from 'src/reward/models/reward-type.enum';
+import { Reward } from 'src/reward/models/reward.model';
+import { RewardService } from 'src/reward/reward.service';
 
 @Processor('hooksQueue')
 export class JobProcessor {
   constructor(
-    private readonly playerService: PlayerService,
-    private readonly submissionService: SubmissionService,
-    private readonly playerRewardService: PlayerRewardService,
-    private readonly challengeStatusService: ChallengeStatusService,
+    protected readonly playerService: PlayerService,
+    protected readonly submissionService: SubmissionService,
+    protected readonly playerRewardService: PlayerRewardService,
+    protected readonly challengeStatusService: ChallengeStatusService,
+    protected readonly rewardService: RewardService,
   ) {}
 
   @Process()
@@ -36,22 +40,22 @@ export class JobProcessor {
       players: () => this.playerService.findAll({ game: { $eq: hook.game } }),
     });
     if (meet) {
-      this.runActions(hook.actions, job.data['params']['playerId']);
+      this.runActions(hook.actions, job.data['params']);
     }
   }
 
-  async runActions(actions: ActionEmbedDto[], playerId: string): Promise<any> {
+  async runActions(actions: ActionEmbedDto[], params: string[]): Promise<any> {
     actions.forEach(action => {
       const props: string[] = action.parameters;
       switch (action.type) {
         case Category.GIVE:
-          this.runGiveActions(props, playerId);
+          this.runGiveActions(props, params['playerId'], params['challengeId'] || null);
           break;
         case Category.TAKE:
-          this.runTakeActions(props, playerId);
+          this.runTakeActions(props, params['playerId']);
           break;
         case Category.UPDATE:
-          this.runUpdateActions(props, playerId);
+          this.runUpdateActions(props, params['playerId'], params['challengeId'] || null);
           break;
       }
     });
@@ -64,7 +68,8 @@ export class JobProcessor {
    * @param parameters - string array
    * @param playerId - id of the player
    */
-  async runGiveActions(parameters: string[] | string, playerId: string) {
+  async runGiveActions(parameters: string[] | string, playerId: string, challengeId?: string) {
+    const reward: Reward = await this.rewardService.findById(parameters[0]);
     const quantity: number = parameters[1] ? +parameters[1] : 1;
     if (parameters[0] !== 'points') {
       await this.playerRewardService.findOneAndUpdate(
@@ -75,6 +80,9 @@ export class JobProcessor {
         { $inc: { count: quantity } },
         { upsert: true, setDefaultsOnInsert: true },
       );
+      if (reward.kind === RewardType.REVEAL) await this.challengeStatusService.markAsAvailable(challengeId, playerId);
+      else if (reward.kind === RewardType.UNLOCK)
+        await this.challengeStatusService.markAsOpen(challengeId, playerId, new Date());
     } else {
       await this.playerService.findOneAndUpdate({ _id: playerId }, { $inc: { points: quantity } });
     }
@@ -112,9 +120,9 @@ export class JobProcessor {
    * @param parameters - string array
    * @param playerId - id of the player
    */
-  async runUpdateActions(parameters: string[], playerId: string) {
+  async runUpdateActions(parameters: string[], playerId: string, challengeId?: string) {
     if (parameters[0] === 'player') {
-      this.updatePlayer(parameters, playerId);
+      this.updatePlayer(parameters, playerId, challengeId);
     } else {
       this.updateChallengeState(parameters, playerId);
     }
@@ -125,21 +133,26 @@ export class JobProcessor {
    */
   async updateChallengeState(param: string[], playerId: string) {
     switch (param[3]) {
+      case State.AVAILABLE:
+        await this.challengeStatusService.markAsAvailable(param[2], playerId);
+        break;
       case State.LOCKED:
+        await this.challengeStatusService.markAsLocked(param[2], playerId);
         break;
       case State.HIDDEN:
+        await this.challengeStatusService.markAsHidden(param[2], playerId);
         break;
       case State.OPENED:
-        this.challengeStatusService.markAsOpen(param[2], playerId, new Date());
+        await this.challengeStatusService.markAsOpen(param[2], playerId, new Date());
         break;
       case State.FAILED:
-        this.challengeStatusService.markAsFailed(param[2], playerId, new Date());
+        await this.challengeStatusService.markAsFailed(param[2], playerId);
         break;
       case State.COMPLETED:
-        this.challengeStatusService.markAsCompleted(param[2], playerId, new Date());
+        await this.challengeStatusService.markAsCompleted(param[2], playerId, new Date());
         break;
       case State.REJECTED:
-        this.challengeStatusService.markAsRejected(param[2], playerId, new Date());
+        await this.challengeStatusService.markAsRejected(param[2], playerId);
         break;
     }
   }
@@ -147,19 +160,33 @@ export class JobProcessor {
   /**
    * Updates player properties.
    */
-  async updatePlayer(param: string[], playerId: string) {
+  async updatePlayer(param: string[], playerId: string, challengeId: string) {
     switch (param[1]) {
       //more cases can be later added
       case 'points':
-        this.updatePlayerPoints(param, playerId);
+        await this.updatePlayerPoints(param, playerId, challengeId);
         break;
     }
+
+    //send CHALLENGE_COMPLETED message to execute attached hooks
+
+    // const hooks: ActionHook[] = await this.actionHookService.findAll({
+    //   $and: [{ trigger: { $eq: TriggerEvent.PLAYER_UPDATED } }, { sourceId: { $eq: challengeId } }],
+    // });
+    // for (const hook of hooks) {
+    //   const job = await this.hooksQueue.add({
+    //     hook: hook,
+    //     params: {
+    //       playerId: playerId
+    //     },
+    //   });
+    // }
   }
 
   /**
    * Updates players points.
    */
-  async updatePlayerPoints(param: string[], playerId: string) {
+  async updatePlayerPoints(param: string[], playerId: string, challengeId: string) {
     if (param[2].startsWith('+')) {
       await this.playerService.findOneAndUpdate({ _id: playerId }, { $inc: { points: +param[2].substring(1) } });
     } else if (param[2].startsWith('-')) {
@@ -167,5 +194,19 @@ export class JobProcessor {
     } else {
       await this.playerService.findOneAndUpdate({ _id: playerId }, { points: +param });
     }
+
+    //send POINTS_UPDATED message to execute attached hooks
+
+    // const hooks: ActionHook[] = await this.actionHookService.findAll({
+    //   $and: [{ trigger: { $eq: TriggerEvent.POINTS_UPDATED } }, { sourceId: { $eq: challengeId } }],
+    // });
+    // for (const hook of hooks) {
+    //   const job = await this.hooksQueue.add({
+    //     hook: hook,
+    //     params: {
+    //       playerId: playerId
+    //     },
+    //   });
+    // }
   }
 }
