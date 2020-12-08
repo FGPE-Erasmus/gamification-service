0c3b20d99adf53c2499fad1fd209ec59e255d004
José C. Paiva <josepaiva94@gmail.com>
2020-12-08 16:02:32 +0000

    remove game enrolled guard

--- src/common/decorators/roles-requiring-enrollment.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { CustomDecorator, SetMetadata } from '@nestjs/common';
-
-export const ROLES_REQUIRING_ENROLLMENT = 'rolesRequiringEnrollment';
-
-export const RolesRequiringEnrollment = (key: string, ...roles: string[]): CustomDecorator =>
-  SetMetadata(ROLES_REQUIRING_ENROLLMENT, [key, ...roles]);
