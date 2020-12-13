5f522f9364feeac1fa162ba186df51a2c5b01c9e
José C. Paiva <josepaiva94@gmail.com>
2020-12-13 20:41:53 +0000

    tests and fixes

--- tools/gen-schema.tool.ts
+++ tools/gen-schema.tool.ts
@@ -6,15 +6,13 @@ import { printSchema } from 'graphql';
 
 import { DateScalar } from '../src/common/scalars/date.scalar';
 import { EmailScalar } from '../src/common/scalars/email.scalar';
-import { KeycloakResolver } from '../src/keycloak/auth.resolver';
-import { UsersResolver } from '../src/users/users.resolver';
 
 async function generateSchema() {
   const app = await NestFactory.create(GraphQLSchemaBuilderModule);
   await app.init();
 
   const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
-  const schema = await gqlSchemaFactory.create([KeycloakResolver, UsersResolver], [DateScalar, EmailScalar]);
+  const schema = await gqlSchemaFactory.create([], [DateScalar, EmailScalar]);
   fs.writeFileSync(path.join(__dirname, '..', 'src', 'schema.gql'), printSchema(schema), 'UTF8');
 }
 
