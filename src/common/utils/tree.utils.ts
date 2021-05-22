6d9f763d4322b6ba01f0f24fa9eaa5c482cb831c
José C. Paiva <josepaiva94@gmail.com>
2021-05-22 10:45:25 +0100

    leaderboard fix

--- src/common/utils/tree.utils.ts
+++ src/common/utils/tree.utils.ts
@@ -29,15 +29,15 @@ export function findSubtree(tree: any[], lookup: any, key = 'id', childrenProp =
   return undefined;
 }
 
-export function collectFromTree(tree: any[], lookupProp: string, childrenProp = 'children', depthFirst = true): any[] {
+export function collectFromTree(tree: any, lookupProp: string, childrenProp = 'children', depthFirst = true): any[] {
   if (!tree || tree.length === 0) {
     return [];
   }
-  const collected = [];
-  for (const node of tree) {
+  const collected = [...tree.refs];
+  for (const node of tree[childrenProp]) {
     collected.push(node[lookupProp]);
 
-    const result = collectFromTree(node[childrenProp], lookupProp, childrenProp, depthFirst);
+    const result = collectFromTree(node, lookupProp, childrenProp, depthFirst);
     collected.push(...result);
   }
   return collected;
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -116,10 +116,9 @@ export class GameService extends BaseService<Game, GameDocument> {
     const zip = gedilFile.content.pipe(Parse({ forceStream: true }));
     for await (const entry of zip) {
       const fileName = entry.path;
-      const buffer = await entry.buffer();
 
       if (fileName === 'metadata.json') {
-        const gedilLayer = extractToJson(buffer);
+        const gedilLayer = extractToJson(await entry.buffer());
         game = await this.create({
           ...input,
           state: GameStateEnum.LOCKED,
@@ -128,16 +127,19 @@ export class GameService extends BaseService<Game, GameDocument> {
           instructors: [teacherId],
         });
       } else {
-        if (!fileName.includes('metadata.json')) continue;
         const result = /^(challenges|leaderboards|rewards|rules)\/([^/]+)\//.exec(fileName);
         if (result) {
+          if (!fileName.includes('metadata.json')) {
+            entry.autodrain();
+            continue;
+          }
           const subpath = fileName.substring(result[0].length);
           if (!entries[result[1]][result[2]]) {
             entries[result[1]][result[2]] = {};
           }
-          entries[result[1]][result[2]][subpath] = buffer;
+          entries[result[1]][result[2]][subpath] = await entry.buffer();
         } else {
-          console.error('Unrecognized entry "%s".', fileName);
+          //console.error('Unrecognized entry "%s".', fileName);
           entry.autodrain();
         }
       }
